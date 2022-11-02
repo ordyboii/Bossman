14fbc43d1c39ef81632ea45838f87b87cea61aa3
Jake <jake.ord345@gmail.com>
2022-11-02 23:17:32 +0000

    first commit

---
+++ app/form.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Form() {
+  const [email, setEmail] = useState("");
+  const [sent, setSent] = useState(false);
+
+  if (sent) {
+    return (
+      <p className='info'>
+        Thanks for requesting to join! Stay tuned for your first edition of
+        juicy news.
+      </p>
+    );
+  }
+
+  return (
+    <>
+      <input
+        type='email'
+        placeholder='Your email'
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+      <a
+        href={`mailto:bossjuicynews@outlook.com?subject=Request to join the Bossman's news from ${email}&body=Request to join`}
+        className='submit'
+        onClick={() => setSent(true)}
+      >
+        Request to join mailing list
+      </a>
+    </>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import Form from "./form";
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <h1>Bossman's Juicy News</h1>
+        <p>
+          The latest <strong>juicy</strong> news from Boss Store, Fenwicks
+        </p>
+      </div>
+      <Form />
+      <em>
+        Be aware of your surroundings, you do not know who's talking to you as a
+        friend or reporter!
+      </em>
+    </main>
+  );
+}
