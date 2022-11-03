423e921ce30aaf7dcdc6cd2744eecc4b72205918
Jake <jake.ord345@gmail.com>
2022-11-03 22:13:32 +0000

    new styling

--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -1,32 +1,25 @@
 import { useState } from "preact/hooks";
 
 export default function Form() {
-  const [email, setEmail] = useState("");
   const [sent, setSent] = useState(false);
 
   if (sent) {
     return (
       <p className='info'>
-        Thanks for requesting to join! Stay tuned for your first edition of
-        juicy news.
+        Thanks for requesting to join! If accepted, stay tuned for your first
+        edition of juicy news.
       </p>
     );
   }
 
   return (
     <>
-      <input
-        type='email'
-        placeholder='Your email'
-        value={email}
-        onChange={e => setEmail(e.currentTarget.value)}
-      />
       <a
-        href={`mailto:bossjuicynews@outlook.com?subject=Request to join the Bossman's news from ${email}&body=Request to join`}
+        href={`mailto:bossjuicynews@gmail.com?subject=Request to join the Bossman's news&body=Request to join the juciest of news`}
         className='submit'
         onClick={() => setSent(true)}
       >
-        Request to join mailing list
+        Send Request to Join
       </a>
     </>
   );
